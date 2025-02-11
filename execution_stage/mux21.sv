e0f28ebc409274ae5729818b1197c3934f86f281
amr-5017 <137155447+amr-5017@users.noreply.github.com>
2025-02-11 17:54:12 +0530

    mux21.sv

--- execution_stage/mux21.sv
+++ execution_stage/mux21.sv
@@ -5,6 +5,6 @@ module mux21
     output logic [31:0] y
 );
 
-    assign y = control ? b : a;
+    assign y = control ? b : a;  //This mux is used to select between the rs2 register data and the immediatae value where the control signal comes from control unit
 
 endmodule
