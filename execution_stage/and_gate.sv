945f3f0917ea37cd0f2b2856b5b614489a5a8e05
amr-5017 <137155447+amr-5017@users.noreply.github.com>
2025-02-11 17:55:58 +0530

    and_gate.sv

--- execution_stage/and_gate.sv
+++ execution_stage/and_gate.sv
@@ -2,5 +2,5 @@ module and_gate(zero,branch,and_out);
 input logic zero;
 input logic branch;
 output logic and_out;
-assign and_out = branch & zero;  
+assign and_out = branch & zero;  //this output acts as the select line for the mux used in IF_Stage for updating the pc value 
 endmodule
